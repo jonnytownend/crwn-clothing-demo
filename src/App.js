ad6120db01ba184177a8daadb9fb02cdd2a887ad
Jonny Townend <jonathan.townend@gmail.com>
2020-04-28 12:11:15 +0100

    Move component files, then create HomePage component template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './components/homepage/homepage.component'
 
 function App() {
   return (
     <div>
+      <HomePage />
     </div>
   );
 }
