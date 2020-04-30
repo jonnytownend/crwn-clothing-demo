f0dc28c3161bd35b360cf516e5594a4609bc03a8
Jonny Townend <jonathan.townend@gmail.com>
2020-04-30 12:12:30 +0100

    Update collection-item button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './collection-item.styles.scss'
-import CollectionPreview from '../collection-preview/collection-preview.component'
+import UnderlineButton from '../../components/underline-button/underline-button.component'
+import BlockButton from '../../components/block-button/block-button.component'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className="collection-item">
         <div className="image" style={{backgroundImage: `url(${imageUrl})`}}>
             <div className="overlay"></div>
         </div>
-        <div className="add-button">ADD TO CART</div>
+        {/* <UnderlineButton className="add-button" text="Add to card" link="/" /> */}
+        <BlockButton className='add-button' value='Add to basket' />
         <div className="collection-footer">
             <span className="name">{name}</span>
             <span className="price">{`£${price}`}</span>
