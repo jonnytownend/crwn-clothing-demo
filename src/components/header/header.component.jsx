7c71180689321a26daf70f0b7da2b23e0fd8ee2f
Jonny Townend <jonathan.townend@gmail.com>
2020-05-01 15:09:41 +0100

    Update header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './header.styles.scss'
+
+import { auth } from '../../firebase/firebase.utils'
+
+import { Link } from 'react-router-dom'
 import { ReactComponent as CrownLogo } from '../../assets/crown-logo.svg'
 import UnderlineButton from '../../components/underline-button/underline-button.component'
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className='header'>
-        <Link to='/' className='logo-container'>
-            <CrownLogo className='logo' />
-        </Link>
+        <div className='logo-container'>
+            <Link to='/' className='logo-container'>
+                <CrownLogo className='logo' />
+            </Link>
+            {currentUser ? <span>{`Hello ${currentUser.displayName}`}</span> : null}
+        </div>
         <div className='links-container'>
-            <UnderlineButton text="Shop" link="shop" />
-            <UnderlineButton text="Contact" link="contact" />
-            <UnderlineButton text="Sign In" link="sign-in" />
+            <Link to='shop'>
+                <UnderlineButton>SHOP</UnderlineButton>
+            </Link>
+            <Link to='contact'>
+                <UnderlineButton>CONTACT</UnderlineButton>
+            </Link>
+            {
+                currentUser ?
+                <UnderlineButton onClick={() => {auth.signOut()}}>SIGN OUT</UnderlineButton> :
+                <Link to='sign-in'>
+                    <UnderlineButton>SIGN IN</UnderlineButton>
+                </Link>
+            }
         </div>
     </div>
 )
