ec89d9253a6fec94a85599b41d6ebf7c0b68234b
Jonny Townend <jonathan.townend@gmail.com>
2020-05-01 15:10:38 +0100

    Update sign in page

--- src/pages/sign-in/sign-in-page.component.jsx
+++ src/pages/sign-in/sign-in-page.component.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './sign-in-page.styles.scss'
 
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 const SignInPage = () => (
     <div className="sign-in-page">
-        <SignIn />
+        <div className='container'>
+            <SignIn />
+            <SignUp />
+        </div>
     </div>
 )
 
